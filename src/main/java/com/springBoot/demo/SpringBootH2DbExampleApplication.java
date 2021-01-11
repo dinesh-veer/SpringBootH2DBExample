bfd728c20827b93b030984a6cb1665e5cf68c68d
dineshveer <dveer123@hotmail.com>
2021-01-11 21:12:32 +0530

    commit for default initilization of DB of H2

---
+++ src/main/java/com/springBoot/demo/SpringBootH2DbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2DbExampleApplication.class, args);
+	}
+
+}
---
+++ target/classes/db/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS CITY ;
+
+ CREATE TABLE CITY (  
+City_code INT AUTO_INCREMENT,  
+city_name VARCHAR2(50) NOT NULL,  
+city_pincode INT(8) NOT NULL);  
